15265d6ce48fd76128632d19b6ba967e9de47c83
Ludwig Sickert <ludwigsickert@gmail.com>
2022-10-04 18:13:48 +0200

    added early exit option to commandline

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import slimstampen.flippingmodel as slim
 import argparse
 
 parser = argparse.ArgumentParser(prog="Slimstampen Flipping model",
-                                 description="runs the slimstampen spacing model extended with a flipping functionality.")
+                                 description="runs the slimstampen spacing model extended with a flipping functionality", epilog="Type `exit`at any time during the run to end immediatly")
 parser.add_argument("--time", "-T", type=int, default=10,
                     help="The number of minutes the program should be run (default: 10)")
 parser.add_argument("--file", "-F", type=str,
@@ -35,6 +35,10 @@ if __name__ == "__main__":
             answer = input(f"New vocabulary: {fact.answer} means {fact.question}!\nPlease type what {fact.question} means below.\n").strip()
         else:
             answer = input(f"What is the translation of {fact.question}?\n").strip()
+
+        if answer == "exit":
+            break
+
         rt = int(time()*1000 - startMs) - presTime
         correct = False
         if answer == fact.answer:
